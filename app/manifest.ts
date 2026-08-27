c6bcd4ab503ddaa1dabad164e98958e08bf5280f
baloerrr04 <akbaradityah444@gmail.com>
2026-08-27 21:33:13 +0700

    feat: add PWA manifest configuration and application icons

--- app/manifest.ts
+++ app/manifest.ts
@@ -17,13 +17,13 @@ export default function manifest(): MetadataRoute.Manifest {
         purpose: 'maskable',
       },
       {
-        src: '/api/icon/192',
+        src: '/icons/icon-192x192.png',
         sizes: '192x192',
         type: 'image/png',
         purpose: 'maskable',
       },
       {
-        src: '/api/icon/512',
+        src: '/icons/icon-512x512.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'maskable',
